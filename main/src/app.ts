9fc76ce6359f1693227b2c4bd78d46ade3cba63a
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-01-16 19:12:52 +0530

    Addding RabbitMQ in both services

--- main/src/app.ts
+++ main/src/app.ts
@@ -3,29 +3,46 @@ import { createConnection } from "typeorm";
 import express from "express";
 import cors from "cors";
 import { Product } from "./entity/product";
-const PORT = 4002;
+import dotenv from "dotenv";
+import amqp from "amqplib/callback_api";
 
+const PORT = 4002;
+dotenv.config();
 createConnection().then((database) => {
   const productRepository = database.getMongoRepository(Product);
+  // console.log("amqpUrls", process.env.AMQP_URL);
+  //@ts-expect-error
 
-  const app: Express = express();
+  amqp.connect(process.env.AMQP_URL, (error: any, connection: any) => {
+    if (error) {
+      throw error;
+    }
+    connection.createChannel((error: any, channel: any) => {
+      if (error) {
+        throw error;
+      }
+      const app: Express = express();
 
-  app.use(
-    cors({
-      origin: [
-        "http://localhost:3000",
-        "http://localhost:8080",
-        "http://localhost:4200",
-      ],
-      methods: "GET, POST, PUT, PATCH, DELETE",
-    })
-  );
-  app.use(express.json());
+      app.use(
+        cors({
+          origin: [
+            "http://localhost:3000",
+            "http://localhost:8080",
+            "http://localhost:4200",
+          ],
+          methods: "GET, POST, PUT, PATCH, DELETE",
+        })
+      );
+      app.use(express.json());
 
-  app.get("/api/products", async (req: Request, res: Response) => {
-    const products = await productRepository.find();
-    return res.json(products);
+      app.get("/api/products", async (req: Request, res: Response) => {
+        const products = await productRepository.find();
+        return res.json(products);
+      });
+
+      app.listen(PORT, () =>
+        console.log(`Main Server is running on PORT ${PORT}`)
+      );
+    });
   });
-
-  app.listen(PORT, () => console.log(`Main Server is running on PORT ${PORT}`));
 });
